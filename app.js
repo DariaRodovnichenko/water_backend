98a7987fbdb90765a99ee02afcfaf190c35d0238
DariaRodovnichenko <125218414+DariaRodovnichenko@users.noreply.github.com>
2024-04-01 22:29:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import "dotenv/config";
 import authRouter from "./routes/api/auth-router.js";
 import waterRouter from "./routes/api/water-router.js";
 
-import swaggerUi from 'swagger-ui-express';
+import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "./swagger/swagger.js";
 
 const app = express();
@@ -18,12 +18,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
-<<<<<<< Updated upstream
-app.use("/api/contacts", contactsRouter);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-=======
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use("/api/water", waterRouter);
->>>>>>> Stashed changes
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
